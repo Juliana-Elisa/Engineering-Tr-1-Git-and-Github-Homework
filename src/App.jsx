5aa143555eda564ea394a6693eb72258858e9a42
Juliana-Elisa <juliana_bracero@elcamino.edu>
2026-07-16 21:31:59 -0700

    i didn't know how to use commint until the end of the assignment, but i put comments for each change within the code that I changed.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
+import './App.css'
+//we need to actually load the file, or the everything will just be plain text.
+
+
 function App() {
   return (
     <div className="container">
     <h1 className="title">Yosemite National Park</h1>
-    <img className="scenery-img-styled" src="./scenery.jpg" alt="A beautiful scenery" />
+    <img className="scenery-img" src="/scenery.jpeg" alt="A beautiful scenery" />
       <p className="description">
         A picture of Yosemite National Park's Half Dome, halfway through the 4 Mile Trail.
       </p>
     </div>
   )
 }
-
+// need to have class name as scenery-img because thats what app.css defines the style for.
+// need to change {sceneryImg} to "/scenery.jpeg" since that is the real path to the image.
 export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import App from './App.jsx' 
+//need to actually define the app so that react knows what the app is.
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
